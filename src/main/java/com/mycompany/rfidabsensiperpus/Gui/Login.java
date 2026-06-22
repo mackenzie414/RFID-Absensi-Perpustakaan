f26f13bde897906db6b655a25a0b80137ffdc246
Tanwirul <tanwirulkhasanah@gmail.com>
2026-06-22 02:42:03 +0100

    update AttandancPage

--- src/main/java/com/mycompany/rfidabsensiperpus/Gui/Login.java
+++ src/main/java/com/mycompany/rfidabsensiperpus/Gui/Login.java
@@ -189,49 +189,44 @@ public class Login extends javax.swing.JFrame {
         
     
         try {
-            //ambil data dari db
             GenericDAO<PetugasPerpus> dao =
                     new GenericDAO<>("users", PetugasPerpus.class);
-            
+
             Bson filter = Filters.eq("username", username);
-            
+
             PetugasPerpus petugas = dao.findOne(filter);
-            
-            
-            //cek user ada atau tidak
+
             if (petugas != null &&
-            SecurityUtils.getHash(password, SecurityUtils.SHA_256).equals(petugas.getPassword())) {
-                
-                // Update waktu login terakhir
+                SecurityUtils.getHash(password, SecurityUtils.SHA_256)
+                        .equals(petugas.getPassword())) {
+
                 petugas.setLastLogin(LocalDateTime.now());
 
-                // Simpan perubahan ke MongoDB
                 dao.update(
                     Filters.eq("username", petugas.getUsername()),
                     petugas
                 );
-            if (petugas !=null &&
-            EncryptionUtils.encrypt(password).equals(petugas.getPassword())) { 
-                
-                JOptionPane.showMessageDialog(this,
-                    "Login Berhasil, selamat datang "
-                    + petugas.getNamaPetugas());
-        
-                        //pindah dashboard
+
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Login Berhasil, selamat datang "
+                        + petugas.getNamaPetugas());
+
                 Dashboard db = new Dashboard();
                 db.setVisible(true);
-                
+
                 this.dispose();
-                
-               
+
             } else {
                 lblError5.setText("Username atau password salah!");
             }
-            
+
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(this,
+            JOptionPane.showMessageDialog(
+                    this,
                     "Error: " + e.getMessage());
         }
+        
      
     }//GEN-LAST:event_MasukActionPerformed
 
